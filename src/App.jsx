56fbe3a8005359628f747940239b32bb2311164c
GhosTHaise <Ghostrex2@gmail.com>
2022-11-02 18:08:38 +0300

    Import assets and build usefull components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+//COMPONENTS
+import {Navbar,Hero, Stats,Business, Billing, CardDeal,Testimonials, Clients, CTA, Footer} from "./components"
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Navbar />
+            </div>
+        </div>
+
+        <section className={`bg-primary ${styles.flexStart}`}>
+              <div className={`${styles.boxWidth}`}>
+                <Hero />
+              </div>
+        </section>  
+
+        <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+              <div className={`${styles.boxWidth}`}>
+                <Stats />
+                <Business />
+                <Billing /> 
+                <CardDeal />
+                <Testimonials /> 
+                <Clients />
+                <CTA />
+                <Footer />
+              </div>
+        </section>  
+    </div>
+  );
+
 
 export default App
---
+++ src/components/FeadbackCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeadbackCard = () => {
+  return (
+    <div>FeadbackCard</div>
+  )
+}
+
+export default FeadbackCard
