b7a9712851a3fe9737e30e0e27205c2f9f94b98a
GhosTHaise <Ghostrex2@gmail.com>
2022-11-03 19:34:22 +0300

    Add Responsive Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import {useState} from 'react'
+import {close,logo,menu} from "../assets"
+import {navLinks} from "../constants"
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  //31:08
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+      <ul  className='list-none sm:flex hidden justify-end items-center flex-1'>
+            {navLinks?.map((nav,index)=>(
+            
+              <li key={nav.id} className={`font-poppins 
+              font-normal 
+              cursor-pointer 
+              text-[16px]
+               text-white
+               ${index == navLinks.length -1 ? "mr-0" : "mr-10"}`}>
+                    <a href={`#${nav.id}`}>
+                      {nav.title} { console.log(index)}
+                    </a>
+              </li>
+            ))}
+      </ul>
+      <div className={`sm:hidden flex flex-1 justify-end items-center`}>
+              <img src={toggle ? close : menu } 
+                      alt="menu" 
+                      className="w-[28px] h-[28px] object-contain cursor-pointer" 
+                      onClick={()=> setToggle((prev) => !prev)}
+              />
+
+              <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                  <ul  className='list-none flex justify-end flex-col items-center flex-1'>
+                      {navLinks?.map((nav,index)=>(
+                      
+                        <li key={nav.id} className={`font-poppins 
+                        font-normal 
+                        cursor-pointer 
+                        text-[16px]
+                        text-white
+                        ${index == navLinks.length -1 ? "mb-0" : "mb-10"}`}>
+                              <a href={`#${nav.id}`}>
+                                {nav.title} { console.log(index)}
+                              </a>
+                        </li>
+                      ))}
+                  </ul>
+              </div>
+      </div>
+    </nav>
   )
 }
 
