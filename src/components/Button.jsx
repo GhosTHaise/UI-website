89e9f46b9cd2790bd596a7d64134f9eccbc79e9d
GhosTHaise <Ghostrex2@gmail.com>
2022-11-05 20:25:11 +0300

    add half bussiness section and create button template

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({styles}) => (
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-md`}>
+        Get Started
+    </button>
+)
 
 export default Button
