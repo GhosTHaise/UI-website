ee9ea7912fb877e98badae181b00edd2ed8793bf
GhosTHaise <Ghostrex2@gmail.com>
2022-11-06 22:42:31 +0300

    Add features to business

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,18 @@ import { features } from "../constants"
 import styles,{layout} from "../style"
 import Button from "./Button"
 
+const FeatureCard = ({icon,title,content,index}) => ( 
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+        <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>   
+            <img src={icon} alt="icon ..." className="w-[50%] h-[50%] object-contain" />
+        </div>
+        <div className="flex-1 flex flex-col ml-3">
+            <h4 className="font-poppins font-semibold text-white test-[18px] leading-[23px]">{title}</h4>
+            <p className="font-poppins font-normeal text-dimWhite test-[16px] leading-[24px] mb-1">{content}</p>
+        </div>
+    </div>
+)
+
 const Business = () => 
 (
     <section id="features" className={layout.section}>
@@ -12,6 +24,13 @@ const Business = () =>
                  But with hundreds of credit cards on the market.
               </p>
               <Button styles={`mt-10`} />
+        </div>
+        <div className={`${layout.sectionImg} flex-col`}>
+            {
+                features.map((feature,index) => ( 
+                    <FeatureCard key={feature.id} {...feature} />
+                ))
+            }
         </div>
     </section>
 )
