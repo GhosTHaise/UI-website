3949a08ce46d43c3c5757e9efc31fb20ee00a68d
GhosTHaise <Ghostrex2@gmail.com>
2022-11-09 21:53:56 +0300

    Start to build testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,22 @@
 import {feedback} from "../constants"
 import styles from "../style"
- 
+import FeadbackCard from "./FeadbackCard" 
 
 const Testimonials = () => 
 ( 
-    <div>Testimonials</div>
+    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+            <div />
+            <div className={`w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]`}>
+                <h1 className={styles.heading2}>
+                    What people are <br className="sm:block hidden" /> saying about us
+                </h1>
+                <div className="w-full md:mt-0 mt-6">
+                    <p className={`${styles.paragraph} text-left maw-width-[470px]`}>
+                        Everything you need to accept card payments and grow your business anywhere on the planet.
+                    </p>
+                </div>
+            </div>
+    </section>
 )
 
 
