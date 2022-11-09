3d5a3b3a6c2faf136cdf12e2f843fc94a0485dc6
GhosTHaise <Ghostrex2@gmail.com>
2022-11-09 23:15:01 +0300

    Finish testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ import FeadbackCard from "./FeadbackCard"
 const Testimonials = () => 
 ( 
     <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-            <div />
+            <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
             <div className={`w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]`}>
                 <h1 className={styles.heading2}>
                     What people are <br className="sm:block hidden" /> saying about us
@@ -15,6 +15,11 @@ const Testimonials = () =>
                         Everything you need to accept card payments and grow your business anywhere on the planet.
                     </p>
                 </div>
+            </div>
+            <div className={`flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1] `}>
+                {feedback.map(card => (
+                    <FeadbackCard key={card.id} {...card} />
+                ))}
             </div>
     </section>
 )
